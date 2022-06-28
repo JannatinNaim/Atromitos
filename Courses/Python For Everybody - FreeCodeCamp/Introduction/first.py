ba4ab7dc0209ef0c0935e6e722aa5b72c3458410
Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-06-28 21:43:25 +0600

    Python - Introduction

---
+++ Courses/Python For Everybody - FreeCodeCamp/Introduction/first.py
@@ -0,0 +1,18 @@
+print("Hello, world!")
+
+x = 5
+x = x + 3
+print(x)
+
+while x > 0:
+    print(x)
+    x = x - 1
+
+print(x)
+
+if x > 10:
+    print("Bigger.")
+else:
+    print("Smaller")
+
+print("Soup")
