79ded5ecd88f33c7f128d0e3a56d0e431c10c75d
Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-06-30 01:10:12 +0600

    Python - Conditional Execution

---
+++ Courses/Python For Everybody - FreeCodeCamp/Conditional Execution/main.py
@@ -0,0 +1,14 @@
+x = 6
+
+if x < 1:
+    print("x is less than 1")
+elif x < 2:
+    print("x is less than 2")
+elif x < 3:
+    print("x is less than 3")
+elif x < 4:
+    print("x is less than 4")
+elif x < 5:
+    print("x is less than 5")
+else:
+    print("x is greater than or equal to 5")
