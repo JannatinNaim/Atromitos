29a3e0e44284c064d45d4d8f8cfa0429979b175a
Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-07-01 03:23:23 +0600

    Demo - Conditionals

---
+++ Demo/index.js
@@ -0,0 +1,13 @@
+// JavaScript
+
+let x = 6;
+
+if (x > 3) {
+    console.log("x is greater than 3");
+
+    if (x > 5) {
+        console.log("x is greater than 5");
+    }
+}
+
+console.log("something");
---
+++ Demo/main.py
@@ -0,0 +1,20 @@
+# Python
+
+x = 6
+
+if x > 3:
+    print("x is greater than 3")
+
+    if x > 5:
+        print("x is greater than 5")
+
+    print("something")
+
+print("something")
+
+
+def some_cool_function(joe_name):
+    print(f"You are really cool, {joe_name}.")
+
+
+some_cool_function("Jannatin Naim")
