b816173d9c165c34766479b5cefef0b48f4f3790
Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-07-05 14:01:40 +0600

    Python - Files

---
+++ Courses/Python For Everybody - FreeCodeCamp/Files/main.py
@@ -0,0 +1,11 @@
+# file = open("mbox.txt")
+# emails = []
+
+# for line in file:
+#     if line.startswith("From: "):
+#         email = line.replace("From: ", "").strip()
+#         if email not in emails:
+#             emails.append(email)
+
+# for email in emails:
+#     print(email)
