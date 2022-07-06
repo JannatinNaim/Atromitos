2e535410540e20c1e9e1510cce909b9eafbfa84c
Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-07-06 18:36:39 +0600

    Comparison

--- Demo/main.py
+++ Demo/main.py
@@ -1,12 +1,9 @@
-# x = []
+x = [1, 2, 3]
+y = [1, 2, 3]
+z = y
 
-# for i in range(1, 101):
-#     if i % 2 == 0:
-#         x.append(i * i)
-#     else:
-#         x.append(i * 2)
+print(x == z)
+print(z == y)
 
-# print(x)
-
-x = [i * i if i % 2 == 0 else i * 2 for i in range(1, 101)]
-print(x)
+z[0] = "asdf"
+print(y)
